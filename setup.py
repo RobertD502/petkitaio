e253296bf3db5175157b16e09acbfd709014a478
Robert Drinovac <52541649+RobertD502@users.noreply.github.com>
2023-02-06 12:33:02 -0500

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="petkitaio",
-    version="0.0.4",
+    version="0.1.0",
     author="Robert Drinovac",
     author_email="unlisted@gmail.com",
     description="Asynchronous Python library for PetKit's API",
