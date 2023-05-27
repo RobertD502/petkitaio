8c1a5e011712cd74c05d1d35a6b07111c79dcd57
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2023-05-27 17:09:50 -0400

    Add ServerError exception

--- petkitaio/exceptions.py
+++ petkitaio/exceptions.py
@@ -24,3 +24,10 @@ class BluetoothError(Exception):
     def __init__(self, *args: Any) -> None:
         """Initialize the exception."""
         Exception.__init__(self, *args)
+
+class ServerError(Exception):
+    """PetKit server error."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
