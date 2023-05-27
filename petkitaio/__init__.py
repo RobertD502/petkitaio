bc85d09c09644b8e1df83d8562266c0dea75ecbe
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2023-05-27 17:06:16 -0400

    add new constants

--- petkitaio/__init__.py
+++ petkitaio/__init__.py
@@ -1,8 +1,8 @@
 from .constants import (
+    AUTH_ERROR_CODES,
     BLE_HEADER,
     BLUETOOTH_ERRORS,
     CLIENT_DICT,
-    ERROR_CODES,
     FEEDER_LIST,
     FeederSetting,
     LB_CMD_TO_KEY,
@@ -17,6 +17,7 @@ from .constants import (
     Header,
     Endpoint,
     Region,
+    SERVER_ERROR_CODES,
     TIMEOUT,
     WATER_FOUNTAIN_LIST,
     W5Command,
@@ -28,13 +29,13 @@ from .constants import (
     W5_SETTINGS_COMMANDS,
 )
 from .petkit_client import (PetKitClient, LOGGER,)
-from .exceptions import (AuthError, BluetoothError, PetKitError,)
+from .exceptions import (AuthError, BluetoothError, PetKitError, ServerError)
 from .model import (Feeder, LitterBox, Pet, PetKitData, W5Fountain, )
 from .str_enum import StrEnum
 
-__all__ = ['AuthError', 'BLE_HEADER', 'BluetoothError', 'BLUETOOTH_ERRORS', 'CLIENT_DICT', 'Feeder', 'Endpoint',
-           'ERROR_CODES', 'FEEDER_LIST', 'FeederSetting', 'Header', 'LB_CMD_TO_KEY', 'LB_CMD_TO_TYPE', 'LB_CMD_TO_VALUE',
+__all__ = ['AuthError', 'AUTH_ERROR_CODES', 'BLE_HEADER', 'BluetoothError', 'BLUETOOTH_ERRORS', 'CLIENT_DICT', 'Feeder', 'Endpoint',
+           'FEEDER_LIST', 'FeederSetting', 'Header', 'LB_CMD_TO_KEY', 'LB_CMD_TO_TYPE', 'LB_CMD_TO_VALUE',
            'LitterBox', 'LitterBoxCommand', 'LitterBoxCommandKey', 'LitterBoxCommandType', 'LitterBoxSetting', 'LITTER_LIST',
-           'LOGGER', 'Pet', 'PetKitClient', 'PetKitData', 'PetKitError', 'PetSetting', 'Region', 'TIMEOUT', 'StrEnum',
+           'LOGGER', 'Pet', 'PetKitClient', 'PetKitData', 'PetKitError', 'PetSetting', 'Region', 'ServerError', 'SERVER_ERROR_CODES', 'StrEnum', 'TIMEOUT',
            'WATER_FOUNTAIN_LIST', 'W5Command', 'W5_COMMAND_TO_CODE', 'W5_DND_COMMANDS', 'W5Fountain', 'W5_LIGHT_BRIGHTNESS',
            'W5_LIGHT_POWER', 'W5_MODE', 'W5_SETTINGS_COMMANDS',  ]
