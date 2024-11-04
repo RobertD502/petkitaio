fd123e379fb0215c3582b9d950387d595ff2a9ba
Dr. Drinovac <52541649+RobertD502@users.noreply.github.com>
2024-11-04 10:04:58 -0500

    temporarily remove D4sh

--- petkitaio/constants.py
+++ petkitaio/constants.py
@@ -267,7 +267,7 @@ SERVER_ERROR_CODES = {
 }
 
 BLE_HEADER = [-6, -4, -3]
-FEEDER_LIST = ['D3', 'D4', 'D4s', 'D4sh', 'Feeder', 'FeederMini']
+FEEDER_LIST = ['D3', 'D4', 'D4s', 'Feeder', 'FeederMini']
 LITTER_LIST = ['T3', 'T4']
 PURIFIER_LIST = ['K2']
 WATER_FOUNTAIN_LIST = ['W5']
